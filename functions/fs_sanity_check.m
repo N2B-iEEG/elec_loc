e73c955086958eedf928eafc20cc36eec7749bd5
qian-chu <qianchu99@gmail.com>
2024-07-17 19:43:46 +0200

    fix sanity check

--- functions/fs_sanity_check.m
+++ functions/fs_sanity_check.m
@@ -1,14 +1,15 @@
 function fs_sanity_check(cfg)
 
-if isfield(cfg, 'dir_fs')
+if ~isfield(cfg, 'dir_fs')
     error('Please specify FreeSurfer directory as cfg.dir_fs')
 elseif ~exist(cfg.dir_fs, "dir")
-    error('%s does not exist\nPlease re-specify FreeSurfer directory at %s', ...
-        cfg.dir_fs, fullfile(elec_loc_dir, 'config.json'))
+    error(['%s does not exist\n' ...
+        'Please specify a valid FreeSurfer directory as cfg.dir_fs'], ...
+        cfg.dir_fs)
 elseif ~exist(fullfile(cfg.dir_fs, 'SetUpFreeSurfer.sh'), 'file')
     error(['%s is not a valid FreeSurfer directory (SetUpFreeSurfer.sh not found)\n' ...
-        'Please re-specify FreeSurfer directory at %s'], ...
-        cfg.dir_fs, fullfile(elec_loc_dir, 'config.json'))
+        'Please specify a valid FreeSurfer directory as cfg.dir_fs'], ...
+        cfg.dir_fs)
 end
 
 end
