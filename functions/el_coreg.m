48fde881902160385c5a75a8528a87e90596903d
Qian Chu <97355086+qian-chu@users.noreply.github.com>
2025-07-03 21:08:53 +0200

    Update el_coreg.m

--- functions/el_coreg.m
+++ functions/el_coreg.m
@@ -3,9 +3,6 @@ function el_coreg(ref_path, source_path, out_path)
 fprintf('>> el_coreg\n\tREFERENCE: %s\n\tSOURCE: %s\n\tOUTPUT: %s\n', ...
     ref_path, source_path, out_path)
 
-fprintf('>>>>>Coregistering %s to %s\n', ...
-    source_path, ref_path)
-
 % Load scans
 t1_spm = spm_vol(char(ref_path));
 other_spm = spm_vol(char(source_path));
