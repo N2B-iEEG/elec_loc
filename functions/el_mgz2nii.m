1d75899fd0163e2cebb9bdacca1c9c56d3e64971
Qian Chu <97355086+qian-chu@users.noreply.github.com>
2025-07-30 13:55:47 +0200

    Update el_mgz2nii.m

--- functions/el_mgz2nii.m
+++ functions/el_mgz2nii.m
@@ -1,24 +1,29 @@
 function el_mgz2nii(cfg, pat)
 
+% Set up FreeSurfer
 bash_code = fs_setup_code(cfg);
-mgz = dir(fullfile(pat.dir.fs_mri, '*.mgz'));
 
-for i_mgz = 1:length(mgz)
-    if startsWith(mgz(i_mgz).name, {'aparc.DKTatlas+aseg', 'aparc.a2009s+aseg', 'wmparc'})
-        i_mgz_path = fullfile(mgz(i_mgz).folder, mgz(i_mgz).name);
-        i_nii_path = fullfile(mgz(i_mgz).folder, [mgz(i_mgz).name(1:end-3) 'nii']);
+% Get all .mgz files in the mri folder
+mgz_files = dir(fullfile(pat.dir.fs_mri, '*.mgz'));
+
+for mgz = mgz_files'
+
+    is_aparc = startsWith(mgz.name, {'aparc.DKTatlas+aseg', 'aparc.a2009s+aseg', 'wmparc'});
+    is_thalamic = startsWith(mgz.name, 'ThalamicNuclei');
+    is_hippo = ~isempty(regexp(mgz.name, 'hippoAmygLabels.*\.v\d+\.mgz$', 'once'));
+
+    if is_aparc || is_hippo || is_thalamic
+        i_mgz_path = fullfile(mgz.folder, mgz.name);
+        i_nii_path = fullfile(mgz.folder, [mgz.name(1:end-3) 'nii']);
         bash_code = sprintf('%s; mri_convert --in_type mgz --out_type nii --out_orientation RAS %s %s', ...
             bash_code, i_mgz_path, i_nii_path);
+
     end
 end
 
-fprintf(['Converting .mgz to .nii in %s\n' ...
-    '================================================================================\n'], ...
-    pat.dir.fs_mri)
-
+% Execute the code through bash
 system(bash_code);
 
-fprintf(['mgz2nii conversion completed\n' ...
-    '================================================================================\n'])
+fprintf('All atlases converted to NIFTI\n')
 
 end
